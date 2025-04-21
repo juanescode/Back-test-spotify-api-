60aa22c9b51ef9322d60854f6f893d418e186839
juanescode <juanestebanvilladagallego23@gmail.com>
2025-04-21 08:20:27 -0500

    feat: /dist

--- dist/index.js
+++ dist/index.js
@@ -20,7 +20,7 @@ app.use((0, morgan_1.default)("common"));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:3000",
     credentials: true
 }));
 app.use("/api", SearchRoutes_1.default);
--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "http://localhost:3000", 
   credentials: true
 }));
 
